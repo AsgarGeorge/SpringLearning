c2e5cf0ca061a86dfc21f1ec8e96286d5246d6eb
Asgar_George <asgarantony@gmail.com>
2024-06-24 11:59:56 +0530

    check commit

--- src/main/java/car/example/bean/MyBean.java
+++ src/main/java/car/example/bean/MyBean.java
@@ -1,2 +1,25 @@
-package car.example.bean;public class MyBean {
+package car.example.bean;
+
+public class MyBean {
+    private String message;
+
+    public MyBean() {
+    }
+
+    public MyBean(String message) {
+        this.message = message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public void showMessage(){
+        System.out.println(message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
