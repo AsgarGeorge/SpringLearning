cd0ee3a4c019763aff77565af24454bbbfe5f0cb
Asgar_George <asgarantony@gmail.com>
2024-06-25 12:18:02 +0530

    new files commited

---
+++ src/main/java/tutorial/tutorial/autowire/autowire/byConstructor/App.java
@@ -0,0 +1,13 @@
+package tutorial.tutorial.autowire.autowire.byConstructor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("AutowireByTypeContext.xml");
+        Car bmw = (Car) context.getBean("myCar");
+        bmw.display();
+
+    }
+}
