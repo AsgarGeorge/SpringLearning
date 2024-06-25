428b27e5386ef961e539a4df62a07eeb41bd0d4d
Asgar_George <asgarantony@gmail.com>
2024-06-25 18:18:46 +0530

    new files commited

--- src/main/java/tutorial/autowire/annotation/Manager.java
+++ src/main/java/tutorial/autowire/annotation/Manager.java
@@ -1,12 +1,15 @@
 package tutorial.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class Manager {
+
     @Autowired
+    @Qualifier("employee")  // if multiple bean have same type we can use qualifier Annotation
     private Employee employee;
 
 
